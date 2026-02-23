2e29bba2b24fb303beae919dcd2f9db35fb43b7e
HenrietteIraguha <h.iraguha@alustudent.com>
2026-02-23 13:39:16 -0800

    corrected an output error on 1-top file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,11 +6,17 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "MyBot/0.1"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json().get("data", {}).get("children", [])
-    for post in posts:
+    data = response.json().get("data", {})
+    posts = data.get("children", [])
+    if not posts:
+        print(None)
+        return
+    for post in posts[:10]:
         print(post.get("data", {}).get("title"))
